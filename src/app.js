1cb56109441a68fd183d1b43c8c42af0d6d3877b
Melisa Rodero Molano <melisa.95.mr@gmail.com>
2025-06-23 21:17:43 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  const suits = ["♦", "♥", "♠", "♣"];
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const value = values[Math.floor(Math.random() * values.length)];
 
+  const suitClass = {
+    "♦": "diamond",
+    "♥": "heart",
+    "♠": "spade",
+    "♣": "club"
+  }[suit];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const card = document.querySelector(".card");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ 
+  card.className = `card ${suitClass}`;
+
+  
+  document.querySelectorAll(".suit").forEach(el => el.textContent = suit);
+  document.querySelectorAll(".value").forEach(el => el.textContent = value);
+}
